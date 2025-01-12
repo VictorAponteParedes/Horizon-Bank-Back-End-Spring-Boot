65e4d035b1aea81e0d9ae86af435e5a61213d396
Victor Aponte <vaponte520@gmail.com>
2025-01-12 16:00:10 -0300

    Agrega: configuracion SecurityFilterChain

--- Horizon-Bank/src/main/java/com/horizonbank/Horizon/Bank/Config/SecurityConfig.java
+++ Horizon-Bank/src/main/java/com/horizonbank/Horizon/Bank/Config/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.horizonbank.Horizon.Bank.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -11,6 +13,18 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable() // Deshabilita CSRF para simplificar pruebas iniciales
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll() // Permite el acceso a todos los endpoints sin autenticación
+                )
+                .httpBasic().disable(); // Deshabilita la autenticación básica
+
+        return http.build();
     }
 
 }
