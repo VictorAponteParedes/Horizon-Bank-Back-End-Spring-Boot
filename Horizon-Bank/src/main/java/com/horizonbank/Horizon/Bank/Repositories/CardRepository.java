0267e2caf0d0c78e0db8648b7132549becb9e30b
Victor Aponte <vaponte520@gmail.com>
2025-01-12 16:05:47 -0300

    Agrega: repository de cardRepository

---
+++ Horizon-Bank/src/main/java/com/horizonbank/Horizon/Bank/Repositories/CardRepository.java
@@ -0,0 +1,10 @@
+package com.horizonbank.Horizon.Bank.Repositories;
+
+import com.horizonbank.Horizon.Bank.Entities.Card;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CardRepository extends JpaRepository<Card, Long> {
+
+}
